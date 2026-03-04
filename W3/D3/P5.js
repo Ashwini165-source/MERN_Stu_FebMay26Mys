7aa56b8d66cfbae2eae4bde25b001b8b039a08de
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-04 13:23:58 +0530

    for loop

---
+++ W3/D3/P5.js
@@ -0,0 +1,9 @@
+//Basics of Objects
+const person = {
+    name: "Rahul",
+    age: 30,
+    isStudent: false
+};
+console.log("person",person);
+console.log("Name:",person.name);
+console.log("Age:",person["age"]);
