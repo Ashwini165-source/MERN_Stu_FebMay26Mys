57fbe160de37075adbb62718530d1f6f88c7e453
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-04 14:41:28 +0530

    JSON stringify parse

--- W3/D3/P8.js
+++ W3/D3/P8.js
@@ -3,5 +3,11 @@ const employee = {
     id:101,
     name: "Sandeep",
     dept: "ME",
-    isActive: true
+    isActive: true,
 };
+const jsonString = JSON.stringify(employee);
+console.log(jsonString);
+
+//JSON parsing
+const parsedObject = JSON.parse(jsonString);
+console.log(parsedObject);
