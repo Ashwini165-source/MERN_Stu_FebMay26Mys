e9bd2146f59a63a64af4227828f143e5a9b03837
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-09 15:04:50 +0530

    Try catch Basics

---
+++ W4/D1/P4.js
@@ -0,0 +1,22 @@
+//Logging
+console.log("Console logging");
+console.warn("Warning message");
+console.error("Error message");
+let users = [
+    { id: 1, name: "Amit" },
+    { id: 2, name: "Santosh" },
+];
+console.log(users);
+console.table(users);
+
+//Groups related logs
+console.group("Grouped logs");
+console.log("Log 1");
+console.log("Log 2");
+console.log("Log 3");
+console.groupEnd();
+
+//measure exection time
+console.time("LoopTimer");
+for(let i=0;i<1000;i++){}
+console.timeEnd("LoopTimer");
