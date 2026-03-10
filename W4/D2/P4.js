724d02e476f1f64441e86f0fa7658e1063cf9a03
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-10 12:50:09 +0530

    P4.js

--- W4/D2/P4.js
+++ W4/D2/P4.js
@@ -5,15 +5,15 @@ const inspectBtn = document.getElementById("inspectBtn");
 const terms = document.getElementById("terms");
 const country = document.getElementById("country");
 
-inspectBtn.addEventListener("click",function(){
-    console.log("Form: ",studentForm);
-    console.log("Name: ",nameInput.value);
-    console.log("Email: ",emailInput.value);
-// radio button
+inspectBtn.addEventListener("click", function () {
+    console.log("Form: ", studentForm);
+    console.log("Name: ", nameInput.value);
+    console.log("Email: ", emailInput.value);
+    // radio button
     const selectedGender = document.querySelector('input[name="gender"]:checked');
-    console.log("Gender: ",selectedGender?selectedGender.value:"Not selected");
+    console.log("Gender: ", selectedGender ? selectedGender.value : "Not selected");
 
     //checkbox
-    console.log("Accepted terms: ",terms.checked);
-    console.log("Country:",country.value);
+    console.log("Accepted terms: ", terms.checked);
+    console.log("Country:", country.value);
 });
