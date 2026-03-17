484577356de5a65f7e56769bfd98bc3470ec60aa
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-17 13:28:53 +0530

    Introduction to callback function

---
+++ W5/D2/P2.js
@@ -0,0 +1,24 @@
+//Basic callback variations
+//1.a callback with no input data
+//2. a callback that receives data from main function
+
+function runTask(Callback){
+    console.log("Task is running.");
+    Callback();
+}
+
+function runTaskWithResult(taskName,callback){
+    console.log("Processing task: ",taskName);
+    callback("Task ",+taskName+"finished successfully");
+}
+
+function showSimpleDoneMessage(){
+    console.log("Simple callback executed.");
+}
+
+function showDetailedMessage(message){
+    console.log(message);
+}
+
+runTask(showSimpleDoneMessage);
+runTaskWithResult("Send monthly report",showDetailedMessage);
---
+++ W5/D2/P5.js
@@ -0,0 +1,18 @@
+//Callback error
+function divideNumbers(a,b,callback){
+    if(b==0){
+        callback("Cannot divide by zero.");
+            return;
+    }
+    const result =a/b;
+    
+    callback(null,result);
+}
+
+divideNumbers(10,0,function(error,result){
+    if(error){
+        console.log("Error: ",error);
+        return;
+    }
+    console.log("Resutt: ",result);
+});
