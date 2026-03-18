1f788228a9fbb954b672a2e754098dd6ce34dc91
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-18 11:59:14 +0530

    Await with non-promise values

---
+++ W5/D3/P6.js
@@ -0,0 +1,19 @@
+// Await with non-promise values
+
+function getReadyValue(){
+    return 25;
+}
+
+function getDelayedValue(){
+    return Promise.resolve(75);
+}
+
+async function showValues(){
+    const readyValue = await getReadyValue();
+    const delayedValue = await getDelayedValue();
+
+    console.log("Ready Values : ",readyValue);
+    console.log("Delayed Values: ",delayedValue);
+}
+
+showValues();
