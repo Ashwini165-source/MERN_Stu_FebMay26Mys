53765940685960e197c435f561a01d1ae19c44fe
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-31 14:33:50 +0530

    Built-in Middleware

---
+++ W7/D1/P8.js
@@ -0,0 +1,5 @@
+//Route parameters and query parameters
+//Route parameters :ncapture dynamic values from the path
+//Query parameters : provides optional values
+const express = require("express");
+const app = express();
