3cc0d533b21c1538b84a6b92a8b02cb48b89052e
Ashwini165-source <ashwinibv2004@gmail.com>
2026-03-31 14:43:40 +0530

    Route parameters and query parameters

--- W7/D1/P8.js
+++ W7/D1/P8.js
@@ -3,3 +3,13 @@
 //Query parameters : provides optional values
 const express = require("express");
 const app = express();
+
+app.get("/products/:id",function(req,res){
+    res.json({
+        routeParameter: req.params.id,
+        queryParameter: req.query
+    });
+});
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+}); 
