3a099c9e4e049adc6e79db8619fe5d9a9dd1107d
Ashwini165-source <ashwinibv2004@gmail.com>
2026-04-01 13:00:14 +0530

    JWT Fundamentals:token generation and verification

--- W7/D3/P5.js
+++ W7/D3/P5.js
@@ -19,10 +19,11 @@ console.log("Header section:",tokenParts[0]);
 console.log("Payload section:",tokenParts[1]);
 console.log("Signature section:",tokenParts[2]);
 console.log("JWT part count:",tokenParts.length);
+const newSecretKey = "Ashwini123";
 
 try{
     //jwt.verify() checks the trust, signature and expiration
-    const verifiedPayload = jwt.verify(token, secretKey);
+    const verifiedPayload = jwt.verify(token,newSecretKey, secretKey);
 
     console.log("verified Payload: ", verifiedPayload);
 } catch (error) {
---
+++ W7/D3/P6.js
@@ -0,0 +1,38 @@
+//Generating token using login function and verifying the token
+const jwt = require("jsonwebtoken");
+
+const secretKey = "Ashu123";
+
+const wrongSecretKey = "Ashwini123";
+function loginUser(email, password) {
+    if (email === "correct@email.com" && password === "mp123") {
+        const token = jwt.sign(
+            {
+                userId: 101,
+                email: email,
+                role: "student"
+            }, secretKey, { expiresIn: "1h" }
+        );
+        return {
+            success: true,
+            token: token
+        };
+    }
+    return {
+        success: false,
+        message: "Invalid Credentials"
+    };
+}
+const loginResult = loginUser("correct@email.com", "mp123");
+console.log("Login result:", loginResult);
+
+if(loginResult.success) {
+    try {
+        const verifiedPayload = jwt.verify(loginResult.token,wrongSecretKey,secretKey);
+        
+            console.log("verified Payload: ", verifiedPayload);
+    }
+    catch (error) {
+    console.error("verification failed:", error.message);
+    }
+}
