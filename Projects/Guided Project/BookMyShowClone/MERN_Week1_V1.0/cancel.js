21f73598ad3175ed1015c7e0a6bfde3b7ec14e13
Ashwini165-source <ashwinibv2004@gmail.com>
2026-04-07 09:14:55 +0530

    To cancel the existing booking if exists

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
@@ -1,8 +1,39 @@
-// This file creates and exports a custom 
-// EventEmitter instance
-const EventEmitter = require("events");
+//cancel.js
+// To cancel the existing booking if exists
+const bookingEmitter = require("./events");
+const {getCurrentBooking,clearCurrentBooking} = require("./booking");
 
-//Custom EventEmitter object
-const bookingEmitter = new EventEmitter();
+function cancelBooking(movies){
+    const booking = getCurrentBooking();
 
-module.exports = bookingEmitter;
+    if (!booking) {
+        bookingEmitter.emit("bookingFailed","No booking found to cancel.");
+        return null;
+    }
+
+    const movie = movies.find((m)=>m.id === booking.movieId);
+    if (!movie) {
+        bookingEmitter.emit("bookingFailed","Movie data not found while cancelling booking.");
+        return null;
+    }
+
+    const showtime = movie.showtimes.find((show)=>show.time.toLowerCase()===booking.time.toLowerCase());
+    if (!showtime) {
+        bookingEmitter.emit("bookingFailed","Showtime data not found");
+        return null;
+    }
+
+    //restore seats
+    showtime.seatsAvailable +=booking.seatCount;
+
+    // clear current Booking
+    clearCurrentBooking();
+
+    bookingEmitter.emit("bookingCancelled",booking);
+
+    return booking;
+}
+
+module.exports = {
+    cancelBooking
+};
