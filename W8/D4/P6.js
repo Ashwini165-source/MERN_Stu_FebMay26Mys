a62c277f1f10df7036c88b35c042d32aafe0ead2
Ashwini165-source <ashwinibv2004@gmail.com>
2026-04-16 14:50:40 +0530

    Generate, store and verify OTP using MongoDB

--- W8/D4/P6.js
+++ W8/D4/P6.js
@@ -1,16 +1,15 @@
-//
-const crypto = require("crypto");
-const mongoose=require("mongoose");
-const { type } = require("os");
+// Generate, store and verify OTP using MongoDB
+const crypto = require('crypto');
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/datedb")
+mongoose.connect('mongodb://127.0.0.1:27017/datedb1')
 .then(()=>console.log("MongoDB connected"))
-.catch((err)=>console.log(err))
+.catch((err)=>console.log(err));
 
 const otpSchema = new mongoose.Schema({
     userId:{
-        type:String,
-        required:true,
+        type: String,
+        required: true,
         unique:true
     },
     otp:{
@@ -27,27 +26,52 @@ const otpSchema = new mongoose.Schema({
         default:0
     }
 });
+
 const OTP = mongoose.model("OTP",otpSchema);
 
-async function generateOTP(userId){
-    const otp= crypto.randomInt(100000, 999999).toString();
+async function generateOTP(userId) {
+    const otp = crypto.randomInt(100000,999999).toString();
 
     await OTP.findOneAndUpdate(
         {userId},
         {
             userId,
-            otp,expiresAt:new Date(Date.now()+6000),
+            otp,
+            expiresAt:new Date(Date.now() + 6000),
             attempts:0
         },
         {
-            upsert:true,
+            upsert: true,
             returnDocument:'after'
         }
     );
     console.log("OTP: ",otp);
 }
 
-async function verifyOTP(userId,enetredOtp){
-    const record = await OTP.findOne({userId}),
-    
+async function verifyOTP(userId,enteredOtp) {
+  const record = await OTP.findOne({userId});
+  
+    if (!record) {return "No OTP";}
+    if(Date.now()>record.expiresAt) return "Expired";
+    if(record.attempts >=3) return "Blocked";
+
+    record.attempts++;
+    if(record.otp === String(enteredOtp)){
+        await OTP.deleteOne({userId});
+        return "Valid";
+    }
+    await record.save();
+    return "Invalid";
 }
+async function main() {
+    // await generateOTP("user1");
+
+
+    const storedRecord = await OTP.findOne({userId:"user1"});
+    const userEnteredOtp = storedRecord.otp;
+
+    const result = await verifyOTP("user1","998579");
+    console.log(result);
+    await mongoose.connection.close();
+}
+main();
