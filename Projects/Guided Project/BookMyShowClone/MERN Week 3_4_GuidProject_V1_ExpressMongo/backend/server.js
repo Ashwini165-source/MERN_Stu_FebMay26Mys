4086807467dc50cf22ec3f60e969701de24f1175
Ashwini165-source <ashwinibv2004@gmail.com>
2026-04-21 10:21:57 +0530

    DB connection function invoked

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -5,7 +5,7 @@ const app = require("./app");
 const connectDB = require("./src/config/db");
 //Connect DB
 connectDB();
-//Port config
+//Port config 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
