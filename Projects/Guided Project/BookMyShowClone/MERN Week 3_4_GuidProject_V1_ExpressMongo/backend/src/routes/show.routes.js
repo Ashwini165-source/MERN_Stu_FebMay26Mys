85bf356a9762703a1c9eee0e797edd00398ed7bc
Ashwini165-source <ashwinibv2004@gmail.com>
2026-04-23 10:51:37 +0530

    Imported show controller and added public and admin only routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/show.routes.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/show.routes.js
@@ -1,1 +1,15 @@
-.
+const express = require("express");
+const router = express.Router();
+const showController = require("../controllers/show.controller");
+const {protect} = require("../middleware/auth.middleware");
+const {authorize} = require("../middleware/role.middleware");
+
+//public route
+router.get("/",showController.getShows);
+router.get("/:id",showController.getShowById);
+
+//Admin only route
+router.post("/",protect,authorize("admin"),showController.createshow);
+router.put("/:id",protect,authorize("admin"),showController.updateshow);
+router.delete("/:id",protect,authorize("admin"),showController.deleteshow);
+module.exports = router;
