41475e7649d6086f8342bffdd3e1727bec51a5d1
Ashwini165-source <ashwinibv2004@gmail.com>
2026-04-23 15:43:49 +0530

    error forwarding method changed

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
@@ -36,16 +36,15 @@ const bookingSchema = new mongoose.Schema({
 });
 
 //Add validation
-bookingSchema.next("save",function () {
+bookingSchema.pre("save",function () {
     if (this.seats.length === 0) {
-        //return next(new Error("At least one seat must be selected"));
+        // return next(new Error("At least one seat must be selected"));
         throw new Error("At least one seat must be selected");
     }
     if(this.totalSeats!=this.seats.length){
-        //return next(new Error("Seat count mismatch"));
+        // return next(new Error("Seat count mismatch"));
         throw new Error("Seat count mismatch");
     }
-    
 });
 //Compound index
 bookingSchema.index({userId:1,showId:1});
