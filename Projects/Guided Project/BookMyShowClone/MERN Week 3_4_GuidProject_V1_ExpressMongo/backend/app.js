dd4314346a99992ae2f662f967ef3b564ab5ef6f
Ashwini165-source <ashwinibv2004@gmail.com>
2026-04-26 21:08:18 +0530

    app.js

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -4,7 +4,8 @@ const movieRoutes = require("./src/routes/movie.routes");
 const bookingRoutes = require("./src/routes/booking.routes");
 const showRoutes = require("./src/routes/show.routes");
 const app = express();
-
+const errorMiddleware = require("./src/middleware/error.middleware"); 
+app.use(errorMiddleware); 
 app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/movies",movieRoutes);
