23b4c2d1da0af6870ba4f107876a705515c221cf
Ashwini165-source <ashwinibv2004@gmail.com>
2026-05-04 12:02:14 +0530

    Event handling with state

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { UseStateBasics } from "./components/P1"
 import { UpdatingObjectsAndArraysState } from './components/P2'
+import {EventHandlingState} from './components/P3'
 
 function App() {
   return (
     <>
       {/* <UseStateBasics /> */}
-      <UpdatingObjectsAndArraysState />
+      {/* <UpdatingObjectsAndArraysState /> */}
+      <EventHandlingState />
     </>
   )
 }
