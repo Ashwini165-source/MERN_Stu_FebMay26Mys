6f91f76cc5eb8071774fb94645a017a8c7c9e32e
Ashwini165-source <ashwinibv2004@gmail.com>
2026-05-07 19:28:21 +0530

    Keyboard events in react

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -17,8 +17,8 @@ function App() {
       {/* <PassingArguments/> */}
       {/* <UseCallbackEvents/> */}
       {/* <EventDelegation/> */}
-      <FormsPattern/>
-      {/* <KeyboardEvent/> */}
+      {/* <FormsPattern/> */}
+      <KeyboardEvent/>
       {/* <EventObj/> */}
       {/* <Accessibility/> */}
     </>
