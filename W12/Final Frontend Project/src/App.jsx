edfb775a5b2467a86f347fd2fae2afdf4d0b2e72
Ashwini165-source <ashwinibv2004@gmail.com>
2026-06-12 12:05:12 +0530

    usage of props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,9 +1,34 @@
 import './App.css'
-import HomePage from "./pages/HomePage";
+// import HomePage from './pages/HomePage'
+import MovieCard from './components/MovieCard'
 function App() {
   return (
-    //Component Composition
-     <HomePage />
+    <>
+    // Component Composition
+    <MovieCard
+      title="Inception"
+      genre="Sci-Fi"
+      rating={4.8}
+      duration="2h 28min"
+      poster="https://picsum.photos/200/300?1"
+    />
+
+    <MovieCard
+      title="Avatar"
+      genre="Sci-Fi"
+      rating={4.4}
+      duration="2h 28min"
+      poster="https://picsum.photos/200/300?2"
+    />
+
+    <MovieCard
+      title="Fight Club"
+      genre="Sci-Fi"
+      rating={4.8}
+      duration="2h 28min"
+      poster="https://picsum.photos/200/300?3"
+    />
+     </>
   );
 }
 
