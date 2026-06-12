1f248e0e871670c9a0a32b97e2a247aace2cffc5
Ashwini165-source <ashwinibv2004@gmail.com>
2026-06-12 12:30:32 +0530

    Statwe concept

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,33 +1,14 @@
 import './App.css'
 // import HomePage from './pages/HomePage'
-import MovieCard from './components/MovieCard'
+// import MovieCard from './components/MovieCard'
+import CitySelector from './components/CitySelector';
+import MovieSearch from './components/MovieSearch';
 function App() {
   return (
     <>
-    // Component Composition
-    <MovieCard
-      title="Inception"
-      genre="Sci-Fi"
-      rating={4.8}
-      duration="2h 28min"
-      poster="https://picsum.photos/200/300?1"
-    />
-
-    <MovieCard
-      title="Avatar"
-      genre="Sci-Fi"
-      rating={4.4}
-      duration="2h 28min"
-      poster="https://picsum.photos/200/300?2"
-    />
-
-    <MovieCard
-      title="Fight Club"
-      genre="Sci-Fi"
-      rating={4.8}
-      duration="2h 28min"
-      poster="https://picsum.photos/200/300?3"
-    />
+    <CitySelector/>
+    {/* Controlled Component */}
+    <MovieSearch/>
      </>
   );
 }
---
+++ W12/Final Frontend Project/src/components/CitySelector.jsx
@@ -0,0 +1,20 @@
+import  {useState} from 'react';
+export default function CitySelector(){
+    const [city,setCity]=useState("Mysuru");
+    return(
+        <section>
+            <h2>Selected City</h2>
+            <p>{city} </p>
+            <button
+                onClick={()=>{
+                    setCity("Mandya");
+                }}>Mandya
+            </button>
+            <button
+                onClick={()=>{
+                    setCity("Bangaluru");
+                }}
+            > Bangaluru</button>
+        </section>
+    )
+}
