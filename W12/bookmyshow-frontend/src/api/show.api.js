795890ed69797e97028be69aa0ab618ad2a14ec4
Ashwini165-source <ashwinibv2004@gmail.com>
2026-06-21 14:14:18 +0530

    Bug fix  : wrt Booked seat details in the seat layout

--- W12/bookmyshow-frontend/src/api/show.api.js
+++ W12/bookmyshow-frontend/src/api/show.api.js
@@ -44,3 +44,21 @@ export async function getShows(filters = {}) {
 }
 
 
+/*
+=========================================================
+GET SHOW BY ID
+
+
+GET /api/shows/:id
+=========================================================
+*/
+
+
+export async function getShowById(showId) {
+  const response = await api.get(
+    `/shows/${showId}`,
+  );
+
+
+  return response.data;
+}
